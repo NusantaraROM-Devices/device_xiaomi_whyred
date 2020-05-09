639fb552cc9b272cea4207db437a195485c50df3
Sonal Singh <sonal.singh.19993@gmail.com>
2020-05-09 19:34:55 +0000

    whyred: Enable Dirac audio effect
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- properties.mk
+++ properties.mk
@@ -32,3 +32,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.sf.lcd_density=440
+
+# Dirac
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dirac.acs.controller=qem \
+    persist.dirac.acs.storeSettings=1 \
+    persist.dirac.acs.ignore_error=1
+
+# Dirac headset effect
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.soundfx.dirac=true \
+    persist.audio.dirac.speaker=true
