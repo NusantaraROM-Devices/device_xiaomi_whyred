c7333938264ec40d70062883ff1ab6fd19ddbab0
Mocarafee <harif.rp05@gmail.com>
2020-08-06 21:34:59 +0700

    whyred: add NusantaraParts

--- nad_whyred.mk
+++ nad_whyred.mk
@@ -22,6 +22,7 @@ $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
 
 # Inherit some common NusantaraROM stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product-if-exists, packages/apps/NusantaraParts/nadproject.mk)
 NAD_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 
