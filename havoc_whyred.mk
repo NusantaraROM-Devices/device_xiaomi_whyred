4fc48cfe79b8c0926713f5e40dc3569d4054d1c7
Kirill Azimov <58362455+Azimxv@users.noreply.github.com>
2020-08-16 21:50:41 +0900

    minor fp updates

--- havoc_whyred.mk
+++ havoc_whyred.mk
@@ -54,6 +54,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=AzimovHero
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
-
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+  
+# Build FP to be picked by both system and vendor
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+PLATFORM_SECURITY_PATCH_OVERRIDE := 2020-08-05
