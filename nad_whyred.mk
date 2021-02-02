3f55deed874464cb4adb097d83116218f6571624
Mocarafee <harif.rp05@gmail.com>
2021-02-02 17:16:47 +0700

    whyred: update redfin fp to February

--- nad_whyred.mk
+++ nad_whyred.mk
@@ -52,4 +52,4 @@ TARGET_VENDOR_PRODUCT_NAME := whyred
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.201205.010 6953398 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
