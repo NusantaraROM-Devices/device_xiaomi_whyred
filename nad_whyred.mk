3be179d73f9f92a7647c8a4abd208ed2f435c226
Mocarafee <harif.rp05@gmail.com>
2021-03-05 05:01:10 +0700

    whyred: Update Redfin FP to March

--- nad_whyred.mk
+++ nad_whyred.mk
@@ -51,6 +51,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.201205.010 6953398 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.2010305.007 7124944 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210305.007/7124944:user/release-keys
