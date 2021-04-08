aa79b45f24aabdc4527f299a6cebd486736dba86
Mocarafee <harif.rp05@gmail.com>
2021-04-09 05:48:13 +0700

    whyred: remove dirac props

--- properties.mk
+++ properties.mk
@@ -33,14 +33,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.hal1.packagelist=com.intsig.camscanner,com.btpn.dc,com.telkom.tracencare \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.faceunlock,com.android.camera
-
-# Dirac
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dirac.acs.controller=qem \
-    persist.dirac.acs.storeSettings=1 \
-    persist.dirac.acs.ignore_error=1
-
-# Dirac headset effect
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.audio.soundfx.dirac=true \
-    persist.audio.dirac.speaker=true
