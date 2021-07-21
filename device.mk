2c835b21b6e8f216cd97c1165ad2c71e24efd2df
SakilMondal <mondalsakil@gmail.com>
2021-07-21 12:47:12 +0530

    whyred: rootdir: fstab: Switch to FBE
    Change-Id: I07450369d0667ac7c91fae97f3239674bc7c57a5

--- device.mk
+++ device.mk
@@ -31,10 +31,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
