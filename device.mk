fe0bd72353585ab416bcbcf25d35ec593e4d1df4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-15 13:50:54 +0800

    whyred: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
