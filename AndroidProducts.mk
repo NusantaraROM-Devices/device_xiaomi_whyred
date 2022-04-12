d26e115c2c8c5c44e9452b975e3abe2187dd07b8
pranshoe <pranshu.goyal71@gmail.com>
2022-04-12 18:38:39 +0530

    whyred: Nuke LOS and Bring Up Potato

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/potato_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    potato_whyred-user \
+    potato_whyred-userdebug \
+    potato_whyred-eng
--- device.mk
+++ device.mk
@@ -16,8 +16,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay 
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
