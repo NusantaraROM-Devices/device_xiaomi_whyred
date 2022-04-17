2cfa481f25197657e262b027fb321820d2ccd675
pranshoe <pranshu.goyal71@gmail.com>
2022-04-17 16:02:51 +0530

    whyred: Add Pixel Launcher Mod
    Credits:
    - @saitamasahil for modifying launcher
    - @MrSluffy_69 & milimnavaUwU for implementing DT2S
    - @lol_nah, @ninopiamonte, @paper_greg, @datnerdguy, @NeFeroN, @m_adrien, @arnavpuranik for making themed icons and sorting them
    - Special thanks to @iamlooper, @Kingsmanz, @xdsolraC & @Suraj_Gangwar
    - @Ardjlon for making the launcher inheritable.
    Original Post: t.me/modulesrepo/3166

--- dot_whyred.mk
+++ dot_whyred.mk
@@ -9,6 +9,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Inherit Pixel Launcher Mod if building with GApps.
+ifneq ($(WITH_GAPPS),true)
+    $(call inherit-product, vendor/PixelMod/PixelMod.mk)
+endif
+
 # Inherit some common Dot stuff
 $(call inherit-product, vendor/dot/config/common.mk)
 
