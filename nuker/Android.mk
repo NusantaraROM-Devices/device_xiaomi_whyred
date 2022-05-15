43d2da33120c32250acd334f981faebded99f7c2
pranshoe <pranshu.goyal71@gmail.com>
2022-05-15 14:56:08 +0530

    whyred: Nuke Android Auto

--- nuker/Android.mk
+++ nuker/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Nuker
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube GoogleTTS SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback RecorderPrebuilt Videos
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube GoogleTTS SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback RecorderPrebuilt Videos AndroidAutoStubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true 
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
