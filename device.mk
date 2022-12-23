430a5c57269f5ddda368cc8be7f53ded2cb707fa
Pranshu Goyal <78131793+datnerdguy@users.noreply.github.com>
2022-12-23 15:27:32 +0000

    Revert "whyred: use libhidlbase from vndk-32 for fingerprint blobs"
    This reverts commit 70ff7c176cfcbc30fe0db7c14ce640a54c3d688e.

--- device.mk
+++ device.mk
@@ -437,10 +437,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 
-# Vndk
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
