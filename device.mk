b3f252900498ed79e188ce3267094d4b24d929c0
dns24 <wahidns24@gmail.com>
2023-01-01 02:11:57 +0700

    whyred: Switch to prebuilt kernel

--- device.mk
+++ device.mk
@@ -285,6 +285,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_VENDOR_KERNEL_HEADERS := device/xiaomi/whyred-kernel/sdm660/kernel-headers
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
